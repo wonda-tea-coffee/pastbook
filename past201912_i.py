0f51eff1ef9659c39d1a6292f0f13f7d4b6ee8ed
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-01-26 11:45:48 +0900

    solve

--- past201912_i.py
+++ past201912_i.py
@@ -2,27 +2,25 @@ N, M = map(int, input().split())
 S = [0]
 C = [0]
 for i in range(M):
-    si, ci = input().split()
-
+    s, c = input().split()
     s_val = 0
     for j in range(N):
-        if si[j] == 'Y':
-            s_val |= 1 << j
-
+        if s[j] == 'Y':
+            s_val |= 1<<j
     S.append(s_val)
-    C.append(int(ci))
+    C.append(int(c))
 
+ALL = 1<<N
 INF = 10**100
-ALL = 1 << N
-dp = [[INF] * ALL for _ in range(M+1)]
-dp[0][0] = 0
-for i in range(1, M+1):
-    for j in range(ALL):
-        dp[i][j] = min(dp[i][j], dp[i-1][j])
-        k = j | S[i]
-        dp[i][k] = min(dp[i][k], dp[i-1][j] + C[i])
+cost = [[INF] * ALL for _ in range(M+1)]
 
-if dp[M-1][ALL-1] == INF:
+cost[0][0] = 0
+for i in range(1, M+1):
+    for n in range(ALL):
+        cost[i][n] = min(cost[i][n], cost[i-1][n])
+        cost[i][n|S[i]] = min(cost[i][n|S[i]], cost[i-1][n] + C[i])
+
+if cost[M][ALL-1] == INF:
     print(-1)
 else:
-    print(dp[M-1][ALL-1])
+    print(cost[M][ALL-1])
