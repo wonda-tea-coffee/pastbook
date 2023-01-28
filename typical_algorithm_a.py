200e0f0fd06b93bed67c8811cd6280613ce09ed8
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-01-29 08:54:25 +0900

    solve

--- typical_algorithm_a.py
+++ typical_algorithm_a.py
@@ -1,15 +1,10 @@
+import bisect
+
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-l = 0
-r = N-1
-ans = -1
-while l < r:
-    m = (l+r)//2
-    if A[m] >= K:
-        r = m
-        ans = m
-    else:
-        l = m+1
-
-print(ans)
+b = bisect.bisect_left(A, K)
+if b == N:
+    print(-1)
+else:
+    print(b)
