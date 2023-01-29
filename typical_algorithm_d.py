d698e5a370f2684dcdea0c0c14f874664d008a63
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-01-29 14:07:29 +0900

    solve

---
+++ typical_algorithm_d.py
@@ -0,0 +1,24 @@
+import heapq
+
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+dist = [-1] * N
+for i in range(M):
+    u, v, c = map(int, input().split())
+    G[u].append((v, c))
+
+Q = []
+heapq.heappush(Q, (0, 0))
+dist[0] = 0
+
+while len(Q) > 0:
+    d, v = heapq.heappop(Q)
+
+    if v == N-1:
+        print(d)
+        break
+
+    for nv, c in G[v]:
+        if dist[nv] == -1 or dist[nv] > d+c:
+            dist[nv] = d+c
+            heapq.heappush(Q, (d+c, nv))
